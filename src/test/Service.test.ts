2a772e5bdb30a0a6e85fa9e9dfea5a91e3df8009
Bart-15 <brat.tabusao@gmail.com>
2024-10-30 20:49:01 +0800

    Local DB Testing with Docker

--- src/test/Service.test.ts
+++
@@ -1,9 +0,0 @@
-import { getCoolName } from '../app/Service';
-
-describe('Service test suite', () => {
-  test('Name should be cool', () => {
-    const actual = getCoolName();
-    const expected = 'VeryCoolName Bart Tabusao';
-    expect(actual).toBe(expected);
-  });
-});
